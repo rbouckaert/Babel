2b569e97839e821cd51ee025eca7733013e5a6d1
Remco Bouckaert <higgs.ml@gmail.com>
2017-09-25 14:57:59 +1300

    add column merge option to CognateSetRandomizer

--- src/babel/tools/CognateSetRandomizer.java
+++ src/babel/tools/CognateSetRandomizer.java
@@ -1,8 +1,10 @@
 package babel.tools;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import beast.util.NexusParser;
@@ -22,7 +24,8 @@ public class CognateSetRandomizer extends Runnable {
 			new File("file.nex"));
 	final public Input<OutFile> outputInput = new Input<>("out", "output file, or stdout if not specified",
 			new OutFile("[[none]]"));
-	final public Input<Double> percentageInput = new Input<>("percentage","percentage of cognatges to knock out", 5.0);
+	final public Input<Double> percentageInput = new Input<>("percentage","percentage of cognates to knock out", 0.0);
+	final public Input<Double> mergePercentageInput = new Input<>("mpercentage","percentage of cognates for which to merge columns", 0.0);
 	final public Input<Boolean> isAscertainedInput = new Input<>("isAscertained", "if true, assume first column in each alignmnet is ascertainemnet column that should not be removed", true);
 
 	@Override
@@ -39,17 +42,28 @@ public class CognateSetRandomizer extends Runnable {
 		}
 	}
 	
+	List<String> taxa;
+	char[][] seqs;
+	Alignment data;
+	NexusParser parser;
+	char[][] seqs2;
+	int [] boundaries0;
+	int [] boundaries;
+	StringBuilder [] newSeqs;
+	int k = 0;
+	int n = 0;
+
 	@Override
 	public void run() throws Exception {
 		if (nexusInput.get() == null || nexusInput.get().getName().equals("[[none]]")) {
 			throw new IllegalArgumentException("A valid nexus file must be specified");
 		}
-		NexusParser parser = new NexusParser();
+		parser = new NexusParser();
 		parser.parseFile(nexusInput.get());
-		Alignment data = parser.m_alignment;
-		List<String> taxa = data.getTaxaNames();
+		data = parser.m_alignment;
+		taxa = data.getTaxaNames();
 		String seq  = data.getSequenceAsString(taxa.get(0));
-		char[][] seqs = new char[seq.length()][data.getTaxonCount()];
+		seqs = new char[seq.length()][data.getTaxonCount()];
 		List<OnePosition> ones = new ArrayList<>();
 		for (int i = 0; i < taxa.size(); i++) {
 			seq  = data.getSequenceAsString(taxa.get(i));
@@ -79,72 +93,49 @@ public class CognateSetRandomizer extends Runnable {
 		}
 		
 		// reconstruct sequences
-		List<Sequence> ss = data.sequenceInput.get();
-		for (int i = 0; i < taxa.size(); i++) {
-			StringBuilder buf = new StringBuilder();
-			for (int j = 0; j < seqs.length; j++) {
-				buf.append(seqs[j][i]);
-			}
-			ss.get(i).dataInput.set(buf.toString());
-		}
-		
-		
-		data.initAndValidate();
-		for (Alignment f : parser.filteredAlignments) {
-			f.initAndValidate();
-		}
+		reconstructSeqs();
 
 		
-		// build new sequences
-		StringBuilder [] newSeqs = new StringBuilder[taxa.size()];
-		for (int i = 0; i < taxa.size(); i++) {
-			newSeqs[i] = new StringBuilder();
+		// merge columns for fraction of ones
+		int mergers = (int) (mergePercentageInput.get() * oneCount / 100.0);
+		if (mergers >= oneCount) {
+			throw new IllegalArgumentException("percentage to mergers should be less than 100%");			
 		}
+		Log.warning("Merging  " + mergers + " ones (" + mergePercentageInput.get() + "% of the ones)");
+
 		
-		int [] boundaries = new int[parser.filteredAlignments.size()];
-		int k = 0;
-		for (Alignment f : parser.filteredAlignments) {
-			int [] pattern = f.getPattern(f.getPatternIndex(0));
-			for (int j = 0; j < taxa.size(); j++) {
-				if (isAscertainedInput.get()) {
-					if (pattern[j] == 0 || pattern[j] == 1) {
-						newSeqs[j].append(" 0");					
-					} else {
-						newSeqs[j].append(" ?");					
-					}
-				} else {
-					newSeqs[j].append(" ");		
+		int merged = 0;
+		while (merged < mergers) {
+			int c1 = Randomizer.nextInt(n);
+			int lo = Arrays.binarySearch(boundaries0, c1);
+			if (lo < 0 || !isAscertainedInput.get()) {
+				if (lo < 0) {
+					lo = - lo - 2;
 				}
-			}
-			
-			for (int i = 0; i < f.getSiteCount(); i++) {
-				boolean include = true;
-				pattern = f.getPattern(f.getPatternIndex(i));
-				if (isAscertainedInput.get() || i > 0) {
-					boolean allZero = true;
-					for (int d : pattern) {
-						if (d == 1) {
-							allZero = false;
-							break;
-						}
-					}
-					include = !allZero;
-				}
-				if (include) {
-					for (int j = 0; j < taxa.size(); j++) {
-						switch (pattern[j]) {
-							case 0: newSeqs[j].append("0");break;
-							case 1: newSeqs[j].append("1");break;
-							default: newSeqs[j].append("?");
-						}
+
+				int hi = boundaries0[lo + 1];
+				int c2 = lo + Randomizer.nextInt(hi - lo) + (isAscertainedInput.get() ? 1 : 0);
+				for (int j = 0; j < taxa.size(); j++) {
+					if (seqs2[j][c1] == '1' || seqs2[j][c2] == '2') {
+						merged++;
+						seqs2[j][c1] = '1';
+						seqs2[j][c2] = '0';
 					}
 				}
 			}
-			boundaries[k] = newSeqs[0].length() - k - 1;
-			k++;
 		}
 		
+				
 		
+		// reconstruct sequences
+		reconstructSeqs2();
+		
+		output();
+
+		Log.warning("Done!");	
+	}
+
+	private void output() throws IOException {
 		StringBuilder charSets = new StringBuilder();
 		int prev = 1;
 		for (k = 0; k < parser.filteredAlignments.size(); k++) {
@@ -178,10 +169,134 @@ public class CognateSetRandomizer extends Runnable {
 		
 		out.print("BEGIN ASSUMPTIONS;\n");
 		out.print(charSets.toString());
-		out.println("END;\n");
+		out.println("END;\n");		
+	}
 
+	private void reconstructSeqs2() {
+		newSeqs = new StringBuilder[taxa.size()];
+		for (int i = 0; i < taxa.size(); i++) {
+			newSeqs[i] = new StringBuilder();
+		}
+		
+		k = 0;
+		boundaries = new int[parser.filteredAlignments.size()];
+		for (int r = 0; r < n; r++) {
+			int [] pattern = getPattern(seqs2, r);
+			if (r == 0 || r == boundaries0[k-1]) {
+				for (int j = 0; j < taxa.size(); j++) {
+					if (isAscertainedInput.get()) {
+						if (pattern[j] == 0 || pattern[j] == 1) {
+							newSeqs[j].append(" 0");					
+						} else {
+							newSeqs[j].append(" ?");					
+						}
+					} else {
+						newSeqs[j].append(" ");		
+					}
+				}
+				if (r > 0) {
+					boundaries[k-1] = newSeqs[0].length() - k-2;
+				}
+				k++;
+			}
+			
+			boolean include = false;
+			for (int d : pattern) {
+				if (d == 1) {
+					include = true;
+					break;
+				}
+			}
 
-		Log.warning("Done!");	}
+			if (include) {
+				for (int j = 0; j < taxa.size(); j++) {
+					switch (pattern[j]) {
+						case 0: newSeqs[j].append("0");break;
+						case 1: newSeqs[j].append("1");break;
+						default: newSeqs[j].append("?");
+					}
+				}
+			}
+		}
+		boundaries[k - 1] = newSeqs[0].length() - k;
+	}
+
+	private void reconstructSeqs() {
+		List<Sequence> ss = data.sequenceInput.get();
+		for (int i = 0; i < taxa.size(); i++) {
+			StringBuilder buf = new StringBuilder();
+			for (int j = 0; j < seqs.length; j++) {
+				buf.append(seqs[j][i]);
+			}
+			ss.get(i).dataInput.set(buf.toString());
+		}
+		
+		
+		data.initAndValidate();
+		for (Alignment f : parser.filteredAlignments) {
+			f.initAndValidate();
+		}
+
+	
+	
+		// build new sequences
+		String seq  = data.getSequenceAsString(taxa.get(0));
+
+		seqs2 = new char[data.getTaxonCount()][seq.length()];
+		boundaries0 = new int[parser.filteredAlignments.size()];
+		for (Alignment f : parser.filteredAlignments) {
+			int [] pattern = f.getPattern(f.getPatternIndex(0));
+			if (isAscertainedInput.get()) {
+				for (int j = 0; j < taxa.size(); j++) {
+					if (pattern[j] == 0 || pattern[j] == 1) {
+						seqs2[j][n] = '0';					
+					} else {
+						seqs2[j][n] = '?';					
+					}
+				}
+				n++;
+			}
+			
+			for (int i = 0; i < f.getSiteCount(); i++) {
+				boolean include = true;
+				pattern = f.getPattern(f.getPatternIndex(i));
+				if (isAscertainedInput.get() || i > 0) {
+					boolean allZero = true;
+					for (int d : pattern) {
+						if (d == 1) {
+							allZero = false;
+							break;
+						}
+					}
+					include = !allZero;
+				}
+				if (include) {
+					for (int j = 0; j < taxa.size(); j++) {
+						switch (pattern[j]) {
+							case 0: seqs2[j][n] = '0';break;
+							case 1: seqs2[j][n] = '1';break;
+							default: seqs2[j][n] = '?';
+						}
+					}
+					n++;
+				}
+			}
+			boundaries0[k] = n;
+			k++;
+		}	
+	}
+
+	private int[] getPattern(char[][] seqs2, int r) {
+		int [] pattern = new int[seqs2.length];
+		for (int i = 0; i < pattern.length; i++) {
+			switch (seqs2[i][r]) {
+			case '0': pattern[i] = 0; break;
+			case '1': pattern[i] = 1; break;
+			default: pattern[i] = -1; break;
+			}
+		}
+		return pattern;
+	}
 
 	public static void main(String[] args) throws Exception {
 		new Application(new CognateSetRandomizer(), "CognateSetRandomizer", args);
