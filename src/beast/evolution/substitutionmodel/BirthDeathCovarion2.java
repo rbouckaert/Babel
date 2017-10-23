002a7c55a6b9f3d4b2cd7f17ff15a727fc3ca8a3
Remco Bouckaert <higgs.ml@gmail.com>
2017-10-23 13:26:06 +1300

    add subst models

---
+++ src/beast/evolution/substitutionmodel/BirthDeathCovarion2.java
@@ -0,0 +1,340 @@
+package beast.evolution.substitutionmodel;
+
+import java.io.PrintStream;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Loggable;
+import beast.core.Input.Validate;
+import beast.core.parameter.RealParameter;
+import beast.evolution.datatype.DataType;
+import beast.evolution.datatype.TwoStateCovarionPlus;
+
+@Description("Covarion model for Binary data with 3 rates")
+public class BirthDeathCovarion2 extends ComplexSubstitutionModel implements Loggable {
+    public Input<RealParameter> switchRateInput = new Input<RealParameter>("switchRate", "the rate of flipping between slow and fast modes", Validate.REQUIRED);
+    public Input<RealParameter> vfrequenciesInput = new Input<RealParameter>("vfrequencies", "the frequencies of the visible states", Validate.REQUIRED);
+    public Input<RealParameter> hfrequenciesInput = new Input<RealParameter>("hfrequencies", "the frequencies of the hidden rates");
+
+    public Input<RealParameter> delParameter = new Input<RealParameter>("deathprob", "rate of death, used to calculate death probability", Validate.REQUIRED);
+    public Input<RealParameter> originLengthInput = new Input<RealParameter>("originLength", "length of the branch at the origin");
+
+    private RealParameter birthDeathRate;
+    private RealParameter switchRate;
+    private RealParameter frequencies;
+    private RealParameter hiddenFrequencies;
+    private RealParameter originLength;
+
+    protected double[][] unnormalizedQ;
+    protected double[][] storedUnnormalizedQ;
+    int stateCount;
+
+    public BirthDeathCovarion2() {
+        ratesInput.setRule(Validate.OPTIONAL);
+        frequenciesInput.setRule(Validate.FORBIDDEN);
+    }
+
+    @Override
+    public void initAndValidate() {
+    	birthDeathRate = delParameter.get();
+        switchRate = switchRateInput.get();
+        frequencies = vfrequenciesInput.get();
+        hiddenFrequencies = hfrequenciesInput.get();
+        originLength = originLengthInput.get();
+
+        
+    	if (switchRate.getDimension() != 2) {
+    		throw new IllegalArgumentException("switchRate should have dimension 2");
+    	}
+        if (birthDeathRate.getDimension() != 2) {
+            throw new IllegalArgumentException("alpha should have dimension 2");
+        }
+        if (frequencies.getDimension() != 3) {
+            throw new IllegalArgumentException("frequencies should have dimension 3");
+        }
+    	if (hfrequenciesInput.get() == null) {
+    		throw new IllegalArgumentException("hiddenFrequenciesshould should be specified");
+    	}
+        if (hiddenFrequencies.getDimension() != 2) {
+            throw new IllegalArgumentException("hiddenFrequenciesshould have dimension 2");
+        }
+        
+        
+        nrOfStates = 5;
+        unnormalizedQ = new double[nrOfStates][nrOfStates];
+        storedUnnormalizedQ = new double[nrOfStates][nrOfStates];
+
+        updateMatrix = true;
+        try {
+			eigenSystem = createEigenSystem();
+		} catch (SecurityException /*| ClassNotFoundException | InstantiationException | IllegalAccessException*/ | IllegalArgumentException
+				/*| InvocationTargetException*/ e) {
+			throw new IllegalArgumentException(e);
+		}
+        rateMatrix = new double[nrOfStates][nrOfStates];
+        relativeRates = new double[nrOfStates * (nrOfStates - 1)];
+        storedRelativeRates = new double[nrOfStates * (nrOfStates - 1)];
+    }
+
+
+    @Override
+    public boolean canHandleDataType(DataType dataType) {
+        return dataType.getClass().equals(TwoStateCovarionPlus.class);
+    }
+
+
+    @Override
+    protected void setupRelativeRates() {
+    }
+
+    
+//    @Override
+//    public void getTransitionProbabilities(Node node, double fStartTime, double fEndTime, double fRate, double[] matrix) {
+//    	super.getTransitionProbabilities(node, fStartTime, fEndTime, fRate, matrix);
+//    	
+//    	for (int i = 0; i < 5; i++) { // sanity check
+//    		double sum = 0;
+//    		for (int j = 0; j < 5; j++) {
+//    			sum += matrix[i*5+j];
+//    		}
+//    		if (Math.abs(sum - 1.0) > 1e-10) {
+//    			int h = 3;
+//    			h++;
+//    		}
+//    	}
+//    }
+
+    @Override
+    protected void setupRateMatrix() {
+        setupUnnormalizedQMatrix();
+
+        for (int i = 0; i < nrOfStates; i++) {
+            for (int j = 0; j < nrOfStates; j++) {
+                rateMatrix[i][j] = unnormalizedQ[i][j];
+            }
+        }
+        // bring in frequencies
+//        for (int i = 0; i < m_nStates; i++) {
+//            for (int j = i + 1; j < m_nStates; j++) {
+//            	m_rateMatrix[i][j] *= fFreqs[j];
+//            	m_rateMatrix[j][i] *= fFreqs[i];
+//            }
+//        }
+        // set up diagonal
+        for (int i = 0; i < nrOfStates; i++) {
+            double fSum = 0.0;
+            for (int j = 0; j < nrOfStates; j++) {
+                if (i != j)
+                    fSum += rateMatrix[i][j];
+            }
+            rateMatrix[i][i] = -fSum;
+        }
+        // normalise rate matrix to one expected substitution per unit time
+        //normalize(rateMatrix, getFrequencies());
+    } // setupRateMatrix
+
+    @Override
+    public double[] getFrequencies() {
+    	if (nrOfStates == 6) {
+	        double[] fFreqs = new double[6];
+	        fFreqs[0] = frequencies.getValue(0) * hiddenFrequencies.getValue(0);
+	        fFreqs[1] = frequencies.getValue(1) * hiddenFrequencies.getValue(0);
+	        fFreqs[2] = frequencies.getValue(2) * hiddenFrequencies.getValue(0);
+	        fFreqs[3] = frequencies.getValue(0) * hiddenFrequencies.getValue(1);
+	        fFreqs[4] = frequencies.getValue(1) * hiddenFrequencies.getValue(1);
+	        fFreqs[5] = frequencies.getValue(2) * hiddenFrequencies.getValue(1);
+	        return fFreqs;
+    	} else {
+	        double [] matrix = new double[nrOfStates * nrOfStates];
+    		getTransitionProbabilities(null, originLength.getValue(), 0, 1.0, matrix);
+	        double[] fFreqs = new double[5];
+	        for (int i = 0; i < 5; i++) {
+	        	fFreqs[i] = (matrix[i] + matrix[15 + i])/2.0;
+	        }
+	        fFreqs[2] = 0;
+    		
+//	        fFreqs[0] = 1e-10;
+//	        fFreqs[1] = 1e-10;
+//	        fFreqs[2] = 1.0 - 4e-10;
+//	        fFreqs[3] = 1e-10;
+//	        fFreqs[4] = 1e-10;
+    		
+//	        double[] fFreqs = new double[5];
+//	        fFreqs[0] = frequencies.getValue(0) * hiddenFrequencies.getValue(0);
+//	        fFreqs[1] = frequencies.getValue(1) * hiddenFrequencies.getValue(0);
+//	        fFreqs[2] = frequencies.getValue(2);
+//	        fFreqs[3] = frequencies.getValue(0) * hiddenFrequencies.getValue(1);
+//	        fFreqs[4] = frequencies.getValue(1) * hiddenFrequencies.getValue(1);
+	        return fFreqs;
+    	}
+    }
+
+
+    protected void setupUnnormalizedQMatrix() {
+
+    	double b = birthDeathRate.getValue(0);
+    	double d = birthDeathRate.getValue(1);
+        double s1 = switchRate.getValue(0);
+        double s2 = switchRate.getValue(1);
+        
+        double slowFactor = 0.00;
+//        double f0 = hiddenFrequencies.getValue(0);
+//        double f1 = hiddenFrequencies.getValue(1);
+        double p0 = frequencies.getValue(0);
+        double p1 = frequencies.getValue(1);
+        double p2 = frequencies.getValue(2);
+//
+//        
+//        
+//        assert Math.abs(1.0 - f0 - f1) < 1e-8;
+//        assert Math.abs(1.0 - p0 - p1 - p2) < 1e-8;
+
+    	if (nrOfStates == 6) {
+	        unnormalizedQ[0][1] = b;// * p1/p1;
+	        unnormalizedQ[0][2] = 0.0;
+	        unnormalizedQ[0][3] = s1;
+	        unnormalizedQ[0][4] = 0.0;
+	        unnormalizedQ[0][5] = 0.0;
+	
+	        unnormalizedQ[1][0] = 0.0;
+	
+	        unnormalizedQ[1][2] = d;//*p0;
+	        unnormalizedQ[1][3] = 0.0;
+	        unnormalizedQ[1][4] = s2;
+	        unnormalizedQ[1][5] = 0.0;
+	        
+	        
+	        unnormalizedQ[2][0] = 0.0;
+	        unnormalizedQ[2][1] = 0.0;
+	
+	        unnormalizedQ[2][3] = 0.0;
+	        unnormalizedQ[2][4] = 0.0;
+	        unnormalizedQ[2][5] = s1;
+	
+	        
+	        unnormalizedQ[3][0] = s1;
+	        unnormalizedQ[3][1] = 0.0;
+	        unnormalizedQ[3][2] = 0.0;
+	
+	        unnormalizedQ[3][4] = b * slowFactor;
+	        unnormalizedQ[3][5] = 0.0;
+	
+	        
+	        unnormalizedQ[4][0] = 0.0;
+	        unnormalizedQ[4][1] = s2;
+	        unnormalizedQ[4][2] = 0.0;
+	        unnormalizedQ[4][3] = 0.0;
+	
+	        unnormalizedQ[4][5] = d * slowFactor;
+	
+	        unnormalizedQ[5][0] = 0.0;
+	        unnormalizedQ[5][1] = 0.0;
+	        unnormalizedQ[5][2] = s1;
+	        unnormalizedQ[5][3] = 0.0;
+	        unnormalizedQ[5][4] = 0.0;
+    	}
+
+    	if (nrOfStates == 5) {
+	    	unnormalizedQ[0][1] = b;// * p1/p1;
+	        unnormalizedQ[0][2] = 0.0;
+	        unnormalizedQ[0][3] = s1;
+	        unnormalizedQ[0][4] = 0.0;
+	
+	        unnormalizedQ[1][0] = 0.0;
+	
+	        unnormalizedQ[1][2] = d;// * p0;
+	        unnormalizedQ[1][3] = 0.0;
+	        unnormalizedQ[1][4] = s2;
+	        
+	        
+	        unnormalizedQ[2][0] = 0.0;
+	        unnormalizedQ[2][1] = 0.0;
+	
+	        unnormalizedQ[2][3] = 0.0;
+	        unnormalizedQ[2][4] = 0.0;
+	
+	        
+	        unnormalizedQ[3][0] = s1;
+	        unnormalizedQ[3][1] = 0.0;
+	        unnormalizedQ[3][2] = 0.0;
+	
+	        unnormalizedQ[3][4] = b * slowFactor;
+	
+	        
+	        unnormalizedQ[4][0] = 0.0;
+	        unnormalizedQ[4][1] = s2;
+	        unnormalizedQ[4][2] = d * slowFactor;
+	        unnormalizedQ[4][3] = 0.0;
+    	}    	
+    }
+
+    /**
+     * Normalize rate matrix to one expected substitution per unit time
+     *
+     * @param matrix the matrix to normalize to one expected substitution
+     * @param pi     the equilibrium distribution of states
+     */
+    private void normalize(double[][] matrix, double[] pi) {
+
+        double subst = 0.0;
+        int dimension = pi.length;
+
+        for (int i = 0; i < dimension; i++) {
+            subst += -matrix[i][i] * pi[i];
+        }
+
+        // normalize, including switches
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                matrix[i][j] = matrix[i][j] / subst;
+            }
+        }
+
+// TODO: work out normalisation
+//        double switchingProportion = 0.0;
+//        switchingProportion += matrix[0][2] * pi[2];
+//        switchingProportion += matrix[2][0] * pi[0];
+//        switchingProportion += matrix[1][3] * pi[3];
+//        switchingProportion += matrix[3][1] * pi[1];
+//        //System.out.println("switchingProportion=" + switchingProportion);
+//
+//        // normalize, removing switches
+//        for (int i = 0; i < dimension; i++) {
+//            for (int j = 0; j < dimension; j++) {
+//                matrix[i][j] = matrix[i][j] / (1.0 - switchingProportion);
+//            }
+//        }
+    }
+
+	@Override
+	public void init(PrintStream out) {
+		out.print("freq1\tfreq2\tfreq3\tfreq4\tfreq5\tsumfreqs\trateb\trated\trates1a\trates1b\trates2a\trates2b\t");
+	}
+
+	@Override
+	public void log(int nSample, PrintStream out) {
+		double [] freqs = getFrequencies();
+		double sum = 0;
+		for (int i = 0; i < freqs.length; i++) {
+			out.print(freqs[i]+"\t");
+			sum += freqs[i];
+		}
+		out.print(sum+"\t");
+		
+		setupRateMatrix();
+		out.print(rateMatrix[0][1] + "\t");
+		out.print(rateMatrix[1][2] + "\t");
+		out.print(rateMatrix[0][3] + "\t");
+		out.print(rateMatrix[3][0] + "\t");
+		out.print(rateMatrix[1][4] + "\t");
+		out.print(rateMatrix[4][1] + "\t");
+	}
+
+	@Override
+	public void close(PrintStream out) {
+		// nothing TO DO 
+		
+	}
+
+
+}
---
+++ src/beast/evolution/substitutionmodel/BirthDeathModel.java
@@ -0,0 +1,205 @@
+package beast.evolution.substitutionmodel;
+
+import beast.core.BEASTInterface;
+import beast.core.Input;
+import beast.core.Input.Validate;
+import beast.core.parameter.RealParameter;
+import beast.evolution.datatype.DataType;
+import beast.evolution.likelihood.TreeLikelihood;
+import beast.evolution.sitemodel.SiteModel;
+import beast.evolution.sitemodel.SiteModelInterface;
+import beast.evolution.tree.Node;
+
+public class BirthDeathModel extends GeneralSubstitutionModel {
+	//public Input<RealParameter> birthRateInput = new Input<RealParameter>("birthrate","rate at which cognates are created", Validate.REQUIRED);
+    public Input<RealParameter> delParameter = new Input<RealParameter>("deathprob", "rate of death, used to calculate death probability", Validate.REQUIRED);
+
+	
+    public BirthDeathModel() {
+    	ratesInput.setRule(Validate.OPTIONAL);
+    }
+    
+
+    @Override
+    public void initAndValidate() {
+        frequencies = frequenciesInput.get();
+        double[] freqs = getFrequencies();
+        nrOfStates = freqs.length;
+        rateMatrix = new double[(nrOfStates)][(nrOfStates)];
+        
+        eigenSystem = new DefaultEigenSystem(nrOfStates);
+    }
+//	@Override
+//	public void getTransitionProbabilities(Node node, double fStartTime, double fEndTime, double fRate, double[] matrix) {
+//		double t = (fStartTime - fEndTime) * fRate;
+//		//{{e^(-a t), e^(a t), 1}, {1, e^(-b t), e^(b t)}, {1, 1, e^t}}
+//		double a = 1.0;//birthRateInput.get().getValue();
+//		double b = delParameter.get().getValue();
+//		matrix[0] = Math.exp(-a*t);
+//		matrix[1] = (1.0 - Math.exp(-a*t)) *  Math.exp(-b*t);
+//		matrix[2] = (1.0 - Math.exp(-a*t)) * (1.0 - Math.exp(-b*t));// -  b/(a-b) * Math.exp(-a*t);
+//		matrix[3] = 0.0;
+//		matrix[4] = Math.exp(-b*t);
+//		matrix[5] = 1.0 - Math.exp(-b*t);
+//		matrix[6] = 0;
+//		matrix[7] = 0;
+//		matrix[8] = Math.exp(-t);
+//
+////		matrix[0] = Math.exp(-a*t);
+////		matrix[1] = (1.0 - Math.exp(-a*t)) *  Math.exp(-b*t);
+////		matrix[2] = (1.0 - Math.exp(-a*t)) * (1.0 - Math.exp(-b*t));// -  b/(a-b) * Math.exp(-a*t);
+////		matrix[3] = 0.0;
+////		
+////		matrix[4] = 0.0;
+////		matrix[5] = Math.exp(-b*t);
+////		matrix[6] = 1.0 - Math.exp(-b*t);
+////		matrix[7] = 0;
+////		
+////		matrix[8] = 0;
+////		matrix[9] = 0;
+////		matrix[10] = Math.exp(-t);
+////		matrix[11] = 0;
+////
+////		matrix[12] = 0;
+////		matrix[13] = 0;
+////		matrix[14] = 0;
+////		matrix[15] = 1.0;
+//	
+//	}
+	
+	/*
+	Q = S.J.S^(-1)
+			where
+			Q = (-a | a  | 0
+			      0 | -b | b
+			      0 | 0  | 0)
+			S = (1 | 1 | a/(a-b)
+			     1 | 0 | 1
+			     1 | 0 | 0)
+			J = (0 | 0  | 0
+			     0 | -a | 0
+			     0 | 0  | -b)
+			S^(-1) = (0 | 0        | 1
+			          1 | -a/(a-b) | -b/(a-b)
+			          0 | 1        | -1)
+			          
+    P(t) = exp(Qt) = S.exp(Jt).S^(-1) = S.{{1,0,0},{0,exp(-at),0},{0,0,exp(-bt)}}.S^(-1)
+    = ( 1 | exp(-at) | exp(-bt) 
+        1 | 0        | 0
+        1 | exp(-at) | 0         ) . S^(-1)
+    = ( exp(-at) |  -a/(a-b) exp(-at) - exp(-bt) | 1 -a/(a-b) exp(-at) -b/(a-b) exp(-bt)  
+        0        | 0                             | 1
+        exp(-at) | -a/(a-b)exp(-at)              | 1 + a/(a-b) exp(-at) b/(a-b)
+    
+    
+    
+    
+    = ( 1        | 1 | 1/(a-b)
+        exp(-at) | 0 | exp(-at)      . S^(-1)
+        exp(-bt) | 0 | 0        )
+    =( exp(-bt) | 0 | 0
+       1 -a/(a-b) exp(-at) + b/(a-b)exp(-bt) | 1 | 1/(a-b) + exp(-at)
+        exp(-at)-exp(-bt) | 0 | exp(-at)
+        
+			          
+			*/
+
+	@Override
+	public EigenDecomposition getEigenDecomposition(Node node) {
+        synchronized (this) {
+            if (updateMatrix) {
+            	double [][] rateMatrix = getRateMatrix();
+
+                eigenDecomposition = eigenSystem.decomposeMatrix(rateMatrix);
+                updateMatrix = false;
+            }
+        }
+        return eigenDecomposition;
+    }
+    
+	protected double[][] getRateMatrix() {
+		if (delParameter.get().getValue() > 1.0) {
+			throw new IllegalArgumentException("death rate should not exceed birthrate (1.0)");
+		}
+		switch (nrOfStates) {
+		case 3:
+		{
+			double [][] rateMatrix = new double[3][3];
+			rateMatrix[0][0] = -1;//- birthRateInput.get().getValue();
+			rateMatrix[0][1] = 1; //birthRateInput.get().getValue();
+			rateMatrix[0][2] = 0;
+			rateMatrix[1][0] = 0;
+			rateMatrix[1][1] = - delParameter.get().getValue();
+			rateMatrix[1][2] = delParameter.get().getValue();
+			rateMatrix[2][0] = 0;
+			rateMatrix[2][1] = 0;
+			rateMatrix[2][2] = 0; 
+			return rateMatrix;
+		}
+		case 4:
+		{
+			double [][] rateMatrix = new double[4][4];
+			rateMatrix[0][0] = -1.0;//- birthRateInput.get().getValue();
+			rateMatrix[0][1] = 1.0;//birthRateInput.get().getValue();
+			rateMatrix[0][2] = 0;
+			rateMatrix[0][3] = 0;
+			
+			rateMatrix[1][0] = 0;
+			rateMatrix[1][1] = - delParameter.get().getValue();
+			rateMatrix[1][2] = delParameter.get().getValue();
+			rateMatrix[1][3] = 0;
+	
+			rateMatrix[2][0] = 0;
+			rateMatrix[2][1] = 0;
+			rateMatrix[2][2] = 0;
+			rateMatrix[2][3] = 0;
+	
+			rateMatrix[3][0] = 0;
+			rateMatrix[3][1] = 0;
+			rateMatrix[3][2] = 0;
+			rateMatrix[3][3] = 0;
+			return rateMatrix;
+		}
+		default :
+			calcStateCount();
+			return getRateMatrix();
+		}
+	}
+
+
+	private void calcStateCount() {
+		for (Object o : getOutputs()) {
+			if (o instanceof SiteModel.Base) {
+				SiteModel.Base s = (SiteModel.Base) o;
+				for (Object o2 : s.getOutputs()) {
+					if (o2 instanceof TreeLikelihood) {
+						TreeLikelihood tl = (TreeLikelihood) o2;
+						DataType dataType = tl.dataInput.get().getDataType();
+						nrOfStates =- dataType.getStateCount();
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void restore() {
+		updateMatrix = true;
+		super.restore();
+	}
+	
+    @Override
+    protected boolean requiresRecalculation() {
+        // we only get here if delParameter or mutationRate is dirty
+    	updateMatrix = true;
+        return true;
+    }
+    
+    
+    @Override
+	public boolean canHandleDataType(DataType dataType) {
+		return (dataType.getStateCount() == 3 || dataType.getStateCount() == 4);
+	}
+
+
+}
