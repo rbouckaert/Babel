eb42c5ffded05eb9fe1eeb1a1e2e1dad9f455597
Remco Bouckaert <higgs.ml@gmail.com>
2018-08-08 10:19:20 +1200

    checkpoint

--- src/babel/tools/LineagesThroughTimeCounter.java
+++ src/babel/tools/LineagesThroughTimeCounter.java
@@ -1,5 +1,6 @@
 package babel.tools;
 
+import java.io.File;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,13 +21,20 @@ import beast.evolution.tree.Tree;
 
 @Description("Produce table for lineages through time plot with 95%HPD bounds")
 public class LineagesThroughTimeCounter extends Runnable {
-	final public Input<TreeFile> treesInput = new Input<>("trees","NEXUS file containing a tree set", Validate.REQUIRED);
-	final public Input<OutFile> outputInput = new Input<>("out","output file. Print to stdout if not specified");
-	final public Input<Integer> burnInPercentageInput = new Input<>("burnin", "percentage of trees to used as burn-in (and will be ignored)", 10);
+	final public Input<List<TreeFile>> treesInput = new Input<>("trees", "NEXUS file containing a tree set",
+			new ArrayList<>(), Validate.REQUIRED);
+	final public Input<OutFile> outputInput = new Input<>("out", "output file. Print to stdout if not specified");
+	final public Input<OutFile> svgOutputInput = new Input<>("svgout", "if specified, produce SVG file with graph");
+	final public Input<Integer> burnInPercentageInput = new Input<>("burnin",
+			"percentage of trees to used as burn-in (and will be ignored)", 10);
 	final public Input<Integer> resolutionInput = new Input<>("resolution", "number of steps in table", 1000);
+	final public Input<Boolean> reverseSVGAxisInput = new Input<>("reverseSVGAxis",
+			"reverse x-axis, that is go forward in time instead of backward", true);
+	final public Input<Double> maxXInput = new Input<>("maxX", "maximum value for x-axis. Automaticlly deduced if < 0", -1.0);
+	final public Input<Double> maxYInput = new Input<>("maxY", "maximum value for y-axis. Automaticlly deduced if < 0", -1.0);
 
 	int N = resolutionInput.get(); // number of steps in history
-	
+
 	@Override
 	public void initAndValidate() {
 	}
@@ -34,72 +42,147 @@ public class LineagesThroughTimeCounter extends Runnable {
 	@Override
 	public void run() throws Exception {
 		N = resolutionInput.get();
-		
+
 		// open file for writing
-        PrintStream out = System.out;
-        if (outputInput.get() != null) {
-        	out = new PrintStream(outputInput.get());
+		PrintStream out = System.out;
+		if (outputInput.get() != null) {
+			out = new PrintStream(outputInput.get());
 			Log.warning("Writing to file " + outputInput.get().getPath());
-        }
+		}
 
-        
-        FastTreeSet trees = new TreeAnnotator().new FastTreeSet(treesInput.get().getAbsolutePath(), burnInPercentageInput.get());
-        trees.reset();
-               
-        
-        List<Integer> [] distrs = new List[N + 1];
-        for (int i = 0; i < distrs.length; i++) {
-        	distrs[i] = new ArrayList<>();
-        }
-        
-        trees.reset();
-        double h = 0;
-        while (trees.hasNext()) {
-        	Tree tree = trees.next();
-        	h = Math.max(tree.getRoot().getHeight(), h);
-        }
-        Log.warning("Maximum height = " + h);
+		for (File treeFile : treesInput.get()) {
 
-        trees.reset();
+			FastTreeSet trees = new TreeAnnotator().new FastTreeSet(treeFile.getAbsolutePath(),
+					burnInPercentageInput.get());
+			trees.reset();
 
-        while (trees.hasNext()) {
-            int [] linCount = new int[N + 1];
-        	Tree tree = trees.next();
-        	for (Node node : tree.getNodesAsArray()) {
-        		if (!node.isRoot()) {
-        			int start = (int) (node.getHeight() * N / h + 0.5);
-        			int end = (int) (node.getParent().getHeight() * N / h + 0.5);
-        			for (int i = start; i <= end; i++) {
-        				linCount[i]++;
-        			}
-        		}
-        	}
-        	for (int i = 0; i < N; i++) {
-        		distrs[i].add(linCount[i]);
-        	}        	
-        }
-        
-        for (int i = 0; i < N; i++) {
-        	out.print(i*h/N + " \t");
-        	List<Integer> counts = distrs[i];
-        	out.print(mean(counts) + "\t");
-        	double [] bounds = bounds(counts);
-        	out.print(bounds[0] + "\t");
-        	out.print(bounds[1] + "\t");        	
-        	out.println();
-        }
-        
-        
-        Log.warning("Done");
-        
-       }
+			List<Integer>[] distrs = new List[N + 1];
+			for (int i = 0; i < distrs.length; i++) {
+				distrs[i] = new ArrayList<>();
+			}
 
+			trees.reset();
+			double h = 0;
+			while (trees.hasNext()) {
+				Tree tree = trees.next();
+				h = Math.max(tree.getRoot().getHeight(), h);
+			}
+			Log.warning("Maximum height = " + h);
+
+			trees.reset();
+
+			while (trees.hasNext()) {
+				int[] linCount = new int[N + 1];
+				Tree tree = trees.next();
+				for (Node node : tree.getNodesAsArray()) {
+					if (!node.isRoot()) {
+						int start = (int) (node.getHeight() * N / h + 0.5);
+						int end = (int) (node.getParent().getHeight() * N / h + 0.5);
+						for (int i = start; i <= end; i++) {
+							linCount[i]++;
+						}
+					}
+				}
+				for (int i = 0; i < N; i++) {
+					distrs[i].add(linCount[i]);
+				}
+			}
+
+			double[][] data = new double[N][4];
+			for (int i = 0; i < N; i++) {
+				data[i][0] = i * h / N;
+				List<Integer> counts = distrs[i];
+				data[i][1] = mean(counts);
+				double[] bounds = bounds(counts);
+				data[i][2] = bounds[0];
+				data[i][3] = bounds[1];
+			}
+
+			smooth(data, 1);
+			smooth(data, 2);
+			smooth(data, 3);
+
+			if (maxXInput.get() > 0) {
+				h = maxXInput.get();
+			}
+			if (reverseSVGAxisInput.get()) {
+				for (int i = 0; i < N; i++) {
+					data[i][0] = h - data[i][0];
+				}
+			}
+
+			out.println("age\tmean\t95%HPD_low\t95%HPD_high");
+			for (int i = 0; i < N; i++) {
+				out.println(data[0] + "\t" + data[1] + "\t" + data[2] + "\t" + data[3]);
+			}
+
+			if (svgOutputInput.get() != null) {
+				double fx = 400 / h;
+				double maxY = 0;
+				if (maxYInput.get() > 0) {
+					maxY = maxYInput.get();
+				} else {
+					for (double[] d : data) {
+						maxY = Math.max(maxY, d[3]);
+					}
+				}
+				double y = 400;
+				double fy = y / maxY;
+
+				PrintStream svg = new PrintStream(svgOutputInput.get());
+				Log.warning("Writing to file " + svgOutputInput.get().getPath());
+				svg.println("<svg xmlns=\"http://www.w3.org/2000/svg\">");
+				svg.println("<style type=\"text/css\">");
+				svg.println(".mean {stroke-width:1;stroke:#0000ff;fill:none;}");
+				svg.println(".intervals {stroke-width:1;opacity:0.2;}");
+				svg.println("</style>");
+
+				svg.println("<rect width='400' height='400' style='fill:none;stroke-width:0.5;stroke:#000;'/>");
+				// mean polyline
+				svg.print("<polyline class=\"mean\" points=\"");
+				for (int i = 0; i < N; i++) {
+					svg.print(data[i][0] * fx + "," + (y - data[i][1] * fy) + " ");
+				}
+				svg.println("\"/>");
+				// 95% hpd polygon
+				svg.print("<polygon class=\"intervals\" points=\"");
+				for (int i = 0; i < N; i++) {
+					svg.print(data[i][0] * fx + "," + (y - data[i][2] * fy) + " ");
+				}
+				for (int i = N - 1; i >= 0; i--) {
+					svg.print(data[i][0] * fx + "," + (y - data[i][3] * fy) + " ");
+				}
+				svg.println("\"/>");
+				svg.println("</svg>");
+				svg.close();
+			}
+		}
+		Log.warning("Done");
+
+	}
+
+	private void smooth(double[][] data, int column) {
+		int window = 5;
+		int N = data.length;
+		double[] smoothed = new double[N];
+		for (int i = window; i < N - window - 1; i++) {
+			double sum = 0;
+			for (int j = i - window; j <= i + window; j++) {
+				sum += data[j][column];
+			}
+			sum /= 2 * window + 1;
+			smoothed[i] = sum;
+		}
+		for (int i = window; i < N - window - 1; i++) {
+			data[i][column] = smoothed[i];
+		}
+	}
 
 	private double[] bounds(List<Integer> counts) {
 		Collections.sort(counts);
 		int lower = counts.size() * 25 / 1000;
 		int upper = counts.size() * 975 / 1000;
-		return new double[]{counts.get(lower), counts.get(upper)};
+		return new double[] { counts.get(lower), counts.get(upper) };
 	}
 
 	private double mean(List<Integer> counts) {
@@ -110,9 +193,8 @@ public class LineagesThroughTimeCounter extends Runnable {
 		return sum / counts.size();
 	}
 
-
 	public static void main(String[] args) throws Exception {
 		new Application(new LineagesThroughTimeCounter(), "LineageThroughTimeCounter", args);
-		
+
 	}
 }
