1bc21c2c715b5553d87c136a9e8243289a6bfd8e
jordandouglas <jordandouglas@windowslive.com>
2020-06-30 10:49:55 +1200

    Illegal tree cleaner tool

---
+++ src/babel/tools/IllegalTreeCleaner.java
@@ -0,0 +1,102 @@
+package babel.tools;
+
+import java.io.PrintStream;
+
+import beast.app.treeannotator.TreeAnnotator;
+import beast.app.treeannotator.TreeAnnotator.MemoryFriendlyTreeSet;
+import beast.app.util.Application;
+import beast.app.util.OutFile;
+import beast.app.util.TreeFile;
+import beast.core.Input;
+import beast.core.Runnable;
+import beast.core.util.Log;
+import beast.core.Input.Validate;
+import beast.evolution.tree.Node;
+import beast.evolution.tree.Tree;
+
+/**
+ * Reports all trees which throw a parsing error and optionally logs the legal subset of trees to a file
+ * @author Jordan Douglas
+ */
+public class IllegalTreeCleaner extends Runnable {
+	final public Input<TreeFile> treesInput = new Input<>("trees","NEXUS file containing a tree set", Validate.REQUIRED);
+	final public Input<OutFile> outputInput = new Input<>("out","output NEXUS file (optional).");
+	
+	
+	
+	@Override
+	public void initAndValidate() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void run() throws Exception {
+		
+
+		// Iterate through trees
+		MemoryFriendlyTreeSet trees = new TreeAnnotator().new MemoryFriendlyTreeSet(treesInput.get().getAbsolutePath(), 0);
+		int i = 0;
+		int badTrees = 0;
+		trees.reset();
+		
+		// Print good trees to output?
+		PrintStream out = null;
+		if (outputInput.get() != null) {
+        	out = new PrintStream(outputInput.get());
+			Log.warning("Writing to file " + outputInput.get().getPath());
+        }
+		
+		Log.warning("Examining trees (count starts at 0) ...");
+		while (trees.hasNext()) {
+	        	
+			try {
+				Tree tree = trees.next();
+				
+				if (tree == null) throw new NullPointerException("Tree is null");
+				
+				
+				if (out != null ) {
+					if (i == 0) {
+						tree.init(out);
+		        		out.println();
+					}
+					
+					out.println();
+		            out.print("tree STATE_" + i + " = ");
+		            final String newick = tree.getRoot().toSortedNewick(new int[1], true);
+		            out.print(newick);
+		            out.print(";");
+				}
+	
+				
+			}catch (Exception e) {
+				Log.warning("Tree number " + i + " cannot be parsed: " + e.getMessage());
+				badTrees ++;
+			}
+			
+			i++;
+	        	
+	        	
+		 }
+		
+		
+	      
+        // Close out file
+		if (out != null ) {
+	        out.println();
+	        out.println("end;");
+	        out.close();
+
+		}
+		
+		Log.warning("Done! " + badTrees + " out of " + i  + " trees are illegal");
+		
+		
+	}
+
+	
+	public static void main(String[] args) throws Exception {
+		new Application(new IllegalTreeCleaner(), "Illegal Tree Cleaner", args);
+	}
+
+}
